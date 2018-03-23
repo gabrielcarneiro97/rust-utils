3368c3b9ccaa53b1f4434ee599b9035a42298e6d
Gabriel Carneiro <gabriel.carneiro97@live.com>
2018-03-23 12:17:26 -0300

    Criei uma função para limpar o cache e outra para imprimir o cache

--- cache-closure.rs
+++ cache-closure.rs
@@ -1,27 +1,38 @@
 use std::collections::HashMap;
 use std::hash::Hash;
 use std::collections::hash_map::Entry;
+use std::fmt::Debug;
 
 struct Cacher< T, P, R>
     where T: Fn(P) -> R {
     function: T,
-    map: HashMap<P, R>
+    cache: HashMap<P, R>
 }
 
 impl<T, P, R> Cacher<T, P, R> 
-    where P: Eq + Hash + Clone, T: Fn(P) -> R {
+    where P: Eq + Hash + Clone + Debug, R: Debug, T: Fn(P) -> R {
 
     fn new(function : T) -> Cacher<T, P, R> {
         Cacher {
             function,
-            map: HashMap::new()
+            cache: HashMap::new()
         }
     }
-    fn value (&mut self, param : P) -> &mut R {
 
-        match self.map.entry(param.clone()) {
+    fn value (&mut self, param : P) -> &mut R {
+        match self.cache.entry(param.clone()) {
             Entry::Occupied(o) => o.into_mut(),
             Entry::Vacant(v) => v.insert((self.function)(param))
+        }
+    }
+
+    fn clear_cache(&mut self) {
+        self.cache = HashMap::new();
+    }
+
+    fn print_cache(self) {
+        for (key, val) in self.cache.iter() {
+            println!("{:?} -> {:?}", key, val);
         }
     }
 }
@@ -36,8 +47,11 @@ fn main () {
         }
         res
     });
+
     println!("{:?}", soma.value(&[10, 11]));
-    println!("{:?}", soma.value(&[10, 11]));
-    println!("{:?}", soma.value(&[10, 11, 13, 14]));
+    println!("{:?}", soma.value(&[10, 12]));
+    println!("{:?}", soma.value(&[10, 13]));
+
+    soma.print_cache();    
         
 }
